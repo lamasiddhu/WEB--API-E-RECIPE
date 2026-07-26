1f0eb589580a837c83140a38e41214726929bcd4
lamasiddhu <siddhulama555@gmail.com>
2026-07-26 21:02:12 +0545

    Sprint 1: add admin user management routes

---
+++ backend/src/routes/admin/user.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { AdminUserController } from "../../controllers/adminUser.controller";
+import { authorizedMiddleware } from "../../middlewares/auth.middleware";
+import { adminMiddleware } from "../../middlewares/admin.middleware";
+import { parseMultipartFields } from "../../middlewares/upload.middleware";
+
+const adminUserRouter = Router();
+const adminUserController = new AdminUserController();
+
+adminUserRouter.use(authorizedMiddleware, adminMiddleware);
+
+adminUserRouter.get("/", adminUserController.getAllUsers);
+adminUserRouter.get("/:id", adminUserController.getUserById);
+adminUserRouter.post("/", adminUserController.createUser);
+adminUserRouter.put("/:id", parseMultipartFields, adminUserController.updateUser);
+adminUserRouter.put("/:id/password", adminUserController.updatePassword);
+adminUserRouter.post("/:id/request-password-reset", adminUserController.requestPasswordReset);
+adminUserRouter.delete("/:id", adminUserController.deleteUser);
+
+export default adminUserRouter;

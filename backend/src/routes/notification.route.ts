1ca10317d69ff6e7d75aa19d63dd93ae16f87d2a
lamasiddhu <siddhulama555@gmail.com>
2026-07-26 21:02:26 +0545

    Sprint 3: add Notification routes

---
+++ backend/src/routes/notification.route.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { NotificationController } from "../controllers/notification.controller";
+import { authorizedMiddleware } from "../middlewares/auth.middleware";
+import { adminMiddleware } from "../middlewares/admin.middleware";
+
+const notificationRouter = Router();
+const notificationController = new NotificationController();
+
+notificationRouter.use(authorizedMiddleware);
+
+notificationRouter.get("/", notificationController.getMyNotifications);
+notificationRouter.post("/pro-request", notificationController.requestPro);
+notificationRouter.patch("/read-all", notificationController.markAllRead);
+notificationRouter.delete("/clear-all", notificationController.clearAll);
+notificationRouter.patch("/:id/read", notificationController.markRead);
+notificationRouter.patch("/:id/respond", adminMiddleware, notificationController.respondToProRequest);
+notificationRouter.post("/announce", adminMiddleware, notificationController.broadcastAnnouncement);
+
+export default notificationRouter;
